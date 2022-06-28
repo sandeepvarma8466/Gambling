c0d2a573b3a8b83019769779a1989bed589fe97f
sandeepvarma8466 <svarma852@gmail.com>
2022-06-28 21:22:06 +0530

    Either win or loss

--- src/main/java/com/blz/gambling/GamblingSimulator.java
+++ src/main/java/com/blz/gambling/GamblingSimulator.java
@@ -1,11 +1,29 @@
 package com.blz.gambling;
 
+import java.util.Random;
+
 public class GamblingSimulator {
 
     static final int stakePerDay = 100;
     static final int betPerGame = 1;
+    static final int isWin = 1;
+    static int stake = 0;
+
+    public static void winCheck() {
+        Random random = new Random();
+        int betReturns = random.nextInt(2);
+        if (betReturns == isWin){
+            stake++;
+            System.out.println("Congratulations you win!!");
+        }
+        else {
+            stake--;
+            System.out.println("You loss the game!!");
+        }
+        System.out.println("Stake: "+(stake+stakePerDay));
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
-        System.out.println("Stack per day: $" +stakePerDay + "\nBet per game: $" +betPerGame);
+        winCheck();
     }
 }
