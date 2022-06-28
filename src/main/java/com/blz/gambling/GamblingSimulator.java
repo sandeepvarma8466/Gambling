ee0c513874d3e8c4b361e4d3f35f7abaa76201a2
sandeepvarma8466 <svarma852@gmail.com>
2022-06-28 21:10:14 +0530

    start with stake

--- src/main/java/com/blz/gambling/GamblingSimulator.java
+++ src/main/java/com/blz/gambling/GamblingSimulator.java
@@ -1,7 +1,11 @@
 package com.blz.gambling;
 
 public class GamblingSimulator {
+
+    static final int stakePerDay = 100;
+    static final int betPerGame = 1;
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
+        System.out.println("Stack per day: $" +stakePerDay + "\nBet per game: $" +betPerGame);
     }
 }
